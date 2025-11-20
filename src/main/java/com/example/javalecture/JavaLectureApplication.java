8ec4a7a55e9c56fa64fc8d8fcd4c8db39327d34f
deadbyshadow <tiringer.mark@gmail.com>
2025-11-20 13:48:12 +0100

    Created the application with springboot and thymeleaf

---
+++ src/main/java/com/example/javalecture/JavaLectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaLecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLectureApplication.class, args);
+	}
+
+}
