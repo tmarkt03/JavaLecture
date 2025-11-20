25fcbcba712ba9a1be9f6a7ba76cbd778fd3b76b
deadbyshadow <tiringer.mark@gmail.com>
2025-11-20 13:53:21 +0100

    Fixed naming scheme

--- src/main/java/com/example/Javalecture/JavaLectureApplication.java
+++ src/main/java/com/example/Javalecture/JavaLectureApplication.java
@@ -1,4 +1,4 @@
-package com.example.JavaLecture;
+package com.example.Javalecture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
